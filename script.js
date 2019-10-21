91d93d25f2b49e93737223bc835676853e775c38
Cleopatra <piperschwartz@aol.com>
2019-10-20 22:11:01 -0600

    commit 1

---
+++ script.js
@@ -0,0 +1,12 @@
+function buildQueryURL() {
+    var queryURL = "api.openweathermap.org/data/2.5/weather?q={denver}";
+
+    var queryParams = { "api-key": "89de3065ee4e2d60005c12c3c47a2525"};
+}
+
+
+
+function updatePage(currentCity) {
+    var currentWeather = $("currentCity").val();
+
+}
