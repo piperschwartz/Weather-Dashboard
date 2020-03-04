2663212ecafcb55f291567ae97d424ff2d7d2b42
piperschwartz <piperschwartz@aol.com>
2020-03-04 12:17:15 -0700

    getting there. Updating the api calls

--- script.js
+++ script.js
@@ -1,46 +1,65 @@
-const apiKey= "89de3065ee4e2d60005c12c3c47a2525";
 
 
-// const moment = moment();
+let cityHistory = JSON.parse(localStorage.getItem("savedCities"));
     
+let cityList= [];
+
 $("#sumbitCity").on("click", function(event) {
+    event.preventDefault();
+    var search = $("#city").val();
     console.log(search);
 
-    event.preventDefault();
-    var search = $("#submitCity").val();
-    listofCities.push(city);
-    displayWeather(search);
-    displayList(search);
+
 });
 
     function displayWeather(search) {
+        var queryURL ="https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=89de3065ee4e2d60005c12c3c47a2525";
+        var forecastURL ="";
+        var uvURL="";
+
         $ajax({
-            url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=89de3065ee4e2d60005c12c3c47a2525",
+            url: queryURL,
             method: "GET",
-            success: function(data) {
-                var display = populate(data);
-                $('#populate').html(display);
-            }
-        });
+        }).then(function(res) {
+            var city = $(".city");
+            var temp = $(".temp");
+            var humidity = $(".humidity");
+            var wind = $(".wind");
+
+            city.html("<h3>" + response.name + moment().format('L') + "</h3>");
+            temp.text("Temperature:" + response.main.temp + "°F");
+            humidity.text("Humidity:" + response.main.humidity + "%");
+            wind.text("Wind Speed" + response.wind.speed + "MPH");
+        })
+        $.ajax({
+            url: uvURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var uv = $(".uv-index");
+
+            uv.text("UV Index:" + response[0].value);
+        })
 
         $.ajax({
-            url: "api.openweathermap.org/data/2.5/forecast?zip={zip code},{country code}" + city + apiKey,
-            method: "GET",
-        success: function(data) {
-            var forecastFive = popForecast(data);
-        }
-        });
+            url: forecastURL,
+            method: "GET"
+        }).then(function(response) {
+            console.log(response);
+            var oneDate = $(".date-1");
+            var oneIcon = $(".icon-1");
+            var oneTemp = $(".temp-1");
+            var oneHumid = $(".humidity-1");
+        })
 
-        $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/uvi?appid=" + "&APPID=89de3065ee4e2d60005c12c3c47a2525" + "&lat=" + lat + "&lon=" + lon,
-            method: "GET"  
-        })
-        .then(function(response) {
-            uvIndex = $("<p>" + "UV Index:" + response.value + "</p>");
-            $(weatherBox).text(uvIndex);
-        })
-    
+
+
+
 };
+
+   
+    
+
 
 function populate(data) {
     return "<h3>" + data.name + moment().format( '(MM/DD/YYYY') + "</h2>" + "<p> Temp:</p>" + $(data.main.temp) + "°F";
